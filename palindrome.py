b19444faadc602914d4a2f89512bd4a5a1aa0ba4
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-15 20:39:00 +0300

    Function to test if a string is a palindrome
    q
    :q

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+import re
+def is_palindrome(string):
+	"""A function that returns true if a  string is a palidrome"""
+	#Convert the string to lowercase
+	string = string.lower()
+
+	#Remove punctuations and spaces using regex
+	string = re.sub("\W+","",string)
+
+	#Reverse the string:
+	count = len(string)
+	new_str = ""
+	while count > 0:
+		new_str +=string[count-1]
+		count-=1
+
+	#Check if string is a palidrome and return true or false
+	if new_str == string:
+		return True
+	else:
+		return False
+
+if __name__ == '__main__':
+	print is_palindrome("Race car")
