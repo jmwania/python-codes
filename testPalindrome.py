0d53221f25aefe1894df58d3f31c2b9f49fee821
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-15 20:52:15 +0300

    A test for the palindrome function

---
+++ testPalindrome.py
@@ -0,0 +1,22 @@
+import unittest
+from palindrome import is_palindrome
+
+class TestPalindrome(unittest.TestCase):
+
+	def test_with_punctuation_and_spaces(self):
+		self.assertEqual(is_palindrome("A Toyota's a Toyota"),True)
+	def test_case_1(self):
+		self.assertEqual(is_palindrome("race car"),True)
+	def test_case_2(self):
+		self.assertEqual(is_palindrome("Red rum, sir, is murder"),True)
+	def test_case_3(self):
+		self.assertEqual(is_palindrome("Eva, can I see bees in a cave?"),True)
+	def test_case_4(self):
+		self.assertEqual(is_palindrome("No lemon, no melon"),True)
+	def test_false_1(self):
+		self.assertEqual(is_palindrome("selfless"),False)
+	def test_false_2(self):
+		self.assertEqual(is_palindrome("sentences"),False)
+
+if __name__ == '__main__':
+	unittest.main()
