0e10ed89be8d9aa02fe9a0b6bf1d8c424ced8e67
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-02-25 22:32:05 +0300

    Add other means of achieving the same function

--- strAndLen.py
+++ strAndLen.py
@@ -1,4 +1,9 @@
 def add_length(str_):
   return map(lambda x: x +" "+str(len(x)),str_.split(" "))
 
+  #Other means
+
+  # def add_length(str_):
+  #   return ["{} {}".format(i, len(i)) for i in str_.split(' ')]
+
 print add_length('you will win')
