3c76f37f8ad0705e809a18a1d74334d4160f87f1
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-02-26 14:31:14 +0300

    Another way of getting string only output

--- cleanedString.py
+++ cleanedString.py
@@ -1,6 +1,14 @@
 def string_clean(s):
-    return filter(lambda a: not a.isdigit(), s)
-  
+    # return filter(lambda a: not a.isdigit(), s)
+    new_str = ""
+    for c in s:
+        for i in "0123456789":
+            if c == i:
+                c = ""
+            else:
+                c = c
+        new_str+=c
+    return new_str
   
   
 
