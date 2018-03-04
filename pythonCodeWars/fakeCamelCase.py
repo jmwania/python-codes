0930aa4b89e30c48e0af4dd23ec509138f0960d1
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-03-04 13:04:43 +0300

    Is this camel case?

---
+++ pythonCodeWars/fakeCamelCase.py
@@ -0,0 +1,14 @@
+#Write simple .camelCase method (camel_case function in PHP or CamelCase in C#) for strings. 
+#All words must have their first letter capitalized without spaces.
+
+def to_camel_case(string):
+    if string == "":
+        return ""
+    else:
+        new_list = []
+        for word in string.split(" "):
+            new_list.append(word.capitalize())
+    return "".join(new_list)
+
+
+print (to_camel_case("test case"))
